ffe25e0bce171fc2e67390fb763b482b66ec2fb4
Sam Freeman <samfreeman8or@gmail.com>
2018-03-28 01:47:17 +0200

    Added title, updated font

--- js/animationTesting.js
+++ js/animationTesting.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $("#test").click(function () {
-        $("#test").toggleClass("rotate");
-        $("#test").animate({
+        $(this).toggleClass("rotate");
+        $(this).animate({
             opacity: 0.0,
             top: "-=50vh",
-        }, 1000);
+        }, 1000, function() {
+            $("h1").fadeIn("slow");
+        });
     });
 });
