b83fc9d436f291cfe6322d9a7decd989be2bb254
Sam Freeman <samfreeman8or@gmail.com>
2018-03-28 02:52:15 +0200

    Updated landing page

--- js/animationTesting.js
+++ js/animationTesting.js
@@ -1,3 +1,4 @@
+// This can be improved with the use of an array and index and delay - will do tomorrow
 $(document).ready(function(){
     $("#test").click(function () {
         $(this).toggleClass("rotate");
@@ -5,7 +6,24 @@ $(document).ready(function(){
             opacity: 0.0,
             top: "-=50vh",
         }, 1000, function() {
-            $("h1").fadeIn("slow");
+            // $("#subcontainer").fadeIn();
+            $("h1").fadeIn("slow", function(){
+                $("#key-points").fadeIn("slow");
+                $("#key-points").animate({
+                    right:"+=31.25vw",
+                }, 1000, function(){
+                    $("#top-projects").fadeIn("slow")
+                    $("#top-projects").animate({
+                        right:"-=25vw",
+                    }, 1000, function(){
+                        $("#education-work").fadeIn("slow");
+                        // $("#education-work").animate({
+                        //     right:"-=31.25.vw",
+                        //     opacity:0.2
+                        // }, 1000);
+                    });
+                });
+            });
         });
     });
 });
